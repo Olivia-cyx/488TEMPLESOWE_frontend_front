ce152f296f761da6e08c41fa50dbf5df795e4203
Yingxin cai <cai19980122@gmail.com>
2023-06-19 16:46:14 +1000

    finish navbar

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import React, { ReactNode } from 'react';
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
+  <html lang="en">
+    <head>
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </head>
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{html,ts,tsx}',
+    './components/**/*.{html,ts,tsx}',
+    './sections/**/*.{html,ts,tsx}',
+    './styles/**/*.{ts,tsx}',
   ],
+  mode: 'jit',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-yellow': '#544E48',
+        'secondary-white': '#DCD5C6',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
       },
     },
   },
   plugins: [],
-}
+};
