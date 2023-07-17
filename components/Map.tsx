59111b140cc3358c2cf7ad76640ec8974044af22
Yingxin cai <cai19980122@gmail.com>
2023-07-17 16:06:32 +1000

    fix:local env

--- components/Map.tsx
+++ components/Map.tsx
@@ -47,63 +47,66 @@ const mapStyles = [
   },
 ]
 
+const googleMapsApiKey = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY
+
 export default function Map() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "",
+    googleMapsApiKey: googleMapsApiKey || "",
   })
   const center = useMemo(() => ({ lat: -37.756959, lng: 145.11248 }), [])
 
   return (
-    <div className="flex mt-40">
-      <div className="App w-[1000px] h-[1034px] ml-40">
+    <div className="flex justify-center items-center mt-40">
+      <div className="App w-[1100px] h-[1034px] ml-40">
         {" "}
         {!isLoaded ? (
           <h1>Loading...</h1>
         ) : (
-          <GoogleMap mapContainerClassName="map-container" center={center} zoom={18} options={{ styles: mapStyles }}>
+          <GoogleMap mapContainerClassName="map-container" center={center} zoom={16} options={{ styles: mapStyles }}>
             <Marker position={{ lat: -37.756959, lng: 145.11248 }} />
           </GoogleMap>
         )}
       </div>
-      <div className="flex-1 ml-20 mr-40 w-[542px] h-[1034px]">
-        <p className="text-[18px]">
-          SPORTS & LEISURE <br />
+      <div className="flex-1 ml-40 mr-40 w-[542px] h-[1034px]">
+        <p className="text-[18px]" style={{ color: "#DCD5C6" }}>
+          <span style={{ fontWeight: "bold", textDecoration: "underline" }}>SPORTS & LEISURE </span>
+          <br />
           01. Manningham Templestowe Leisure Centre <br />
           02.Westerfolds Sport Centre <br />
           03.Rosanna Golf Course <br />
           04.The Aquarena Aquatic and Leisure Centre <br />
           05.Eastern Golf Club <br />
           <br />
-          SOMETHING GREEN <br />
+          <span style={{ fontWeight: "bold", textDecoration: "underline" }}>SOMETHING GREEN</span> <br />
           21.Westerfolds Park
           <br />
           22.Koonung Creek Reserve <br />
           23.Finns Reserve <br />
-          24.Templestowe Reserve 25.Main Yarra Trail
+          24.Templestowe Reserve <br /> 25.Main Yarra Trail
           <br /> 26. Ruffey Creek Linear Park <br />
           <br />
-          SHOPPING <br />
+          <span style={{ fontWeight: "bold", textDecoration: "underline" }}>SHOPPING</span> <br />
           11.Templestowe Village SC <br />
           12.The Pines Shopping Centre
           <br /> 13.Westfield Doncaster <br />
           14.Macedon Plaza
           <br />
-          <br /> BUS ROUTE
+          <br /> <span style={{ fontWeight: "bold", textDecoration: "underline" }}>BUS ROUTE </span>
           <br /> 309: City - Donvale <br />
           281: Templestowe - Deakin University
           <br /> 282: Templestowe Village SC, Doncaster SC
           <br /> 280: Tunstall Square SC, Doncaster <br />
           <br />
-          QUALITY SCHOOLS <br />
+          <span style={{ fontWeight: "bold", textDecoration: "underline" }}>QUALITY SCHOOLS </span>
+          <br />
           31.St Kevin’s Primary school
           <br /> 32.Templestowe Valley Primary School <br />
-          33.Templestowe Heights
+          33.Templestowe HeightsPrimary School
           <br />
-          <br /> Primary School <br />
           34.St Gregory The Great Primary
           <br /> 35.Templestowe College
           <br />
-          <br /> MEDICAL CENTRES <br />
+          <br /> <span style={{ fontWeight: "bold", textDecoration: "underline" }}>MEDICAL CENTRES</span> <br />
           41.Foote Street Medical Centre
           <br /> 42.Doncaster-Templestowe Medical Centre <br />
           43.Manningham Medical Centre
