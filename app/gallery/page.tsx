f0dcca1fd80b6e8cabf250b26491b83dd0787f4f
Yingxin cai <cai19980122@gmail.com>
2023-07-18 18:40:31 +1000

    gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,17 +1,38 @@
+import { images } from "../../constants/gallery"
+import styles from "../../styles"
+
+type ImageProps = {
+  alt: string
+  src: string
+  size: "full" | "half"
+}
+const Image: React.FC<ImageProps> = ({ src, alt, size }) => {
+  const widthClass = size === "full" ? "w-full" : "w-1/2"
+
+  return (
+    <div className={`${widthClass} p-1 md:p-2`}>
+      <img alt={alt} className="block h-full w-full object-cover object-center" src={src} />
+    </div>
+  )
+}
+
 const GalleryPage: React.FC = () => (
-  <div className="bg-primary-yellow overflow-hidden h-screen">
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
-    <h2>gallery</h2>
+  <div className={`bg-primary-yellow overflow-hidden ${styles.height3256}`}>
+    <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 mt-40">
+    <Image src="kitchen.png" alt="gallery" size="full" />
+      <div className="-m-1 flex flex-wrap md:-m-2">
+        <div className="flex w-1/2 flex-wrap">
+          <Image src="kitchen.png" alt="gallery" size="half" />
+          <Image src="Bathroom image.png" alt="gallery" size="half" />
+          <Image src="bar_upstairs.png" alt="gallery" size="full" />
+        </div>
+        <div className="flex w-1/2 flex-wrap">
+          <Image src="living_Room.png" alt="gallery" size="full" />
+          <Image src="Mask Group 6.png" alt="gallery" size="half" />
+          <Image src="Mask Group 13.png" alt="gallery" size="half" />
+        </div>
+      </div>
+    </div>
   </div>
 )
 
