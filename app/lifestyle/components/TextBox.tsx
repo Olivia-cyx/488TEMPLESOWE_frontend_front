bd22e8f9227168a5b83ca2fbce36be721192a51a
Adamyyyy <happyadam2008@gmail.com>
2023-07-22 00:38:27 +1000

    fix

--- app/lifestyle/components/TextBox.tsx
+++ app/lifestyle/components/TextBox.tsx
@@ -7,7 +7,7 @@ interface TextProps {
 const TextBox: React.FC<TextProps> = ({ content, textStyles }) => {
   return (
     <div
-      className=" md:w-[400px] md:h-[160px] w-[180px] h-[230px] p-6 absolute before:absolute before:w-10 before:h-10 before:content-[''] before:top-0 before:left-0 before:border-l-4 before:border-t-4 after:absolute after:w-10 after:h-10 after:content-[''] after:right-0 after:bottom-0 after:border-r-4 after:border-b-4 transition-all duration-500 ease-in-out"
+      className=" md:w-[400px] md:h-[160px] w-[180px] h-[230px] p-6 absolute before:absolute before:w-10 before:h-10 before:content-[''] before:top-0 before:left-0 before:border-l-4 before:border-t-4 after:absolute after:w-10 after:h-10 after:content-[''] after:right-0 after:bottom-0 after:border-r-4 after:border-b-4 transition-all duration-500 ease-in-out textBoxRsponsive"
       style={{ ...textStyles }}
     >
       <p className="text-white md:text-lg text-sm">{content}</p>
