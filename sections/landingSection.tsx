f543ea1acd6ddc180dadb02c3fbe3440f3cf3954
Yingxin cai <cai19980122@gmail.com>
2023-07-24 12:42:06 +1000

    mobile

--- sections/landingSection.tsx
+++ sections/landingSection.tsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
   return (
     <motion.div className="flex justify-center items-center flex-col ">
       <div className="xl:mt-5 xl:pt-0 md:mt-10 pt-20 ">
-        <img src="Titlepage logo.png" alt="logo" className="xl:w-[710px] xl:h-[700px] xl:mt-5 xl:pt-0 object-center md:mt-60 pt-20 w-[350px] h-[380px]" />
+        <img src="Titlepage logo.png" alt="logo" className="xl:w-[710px] xl:h-[700px] xl:mt-5 xl:pt-0 object-center md:mt-60 pt-20 w-[350px] h-[390px]" />
       </div>
 
       <div className="py-20 flex items-center">
